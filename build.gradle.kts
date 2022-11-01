9eaeeb90f5dd1134457445ce45101eabbc1ffab9
Robin Mauritz <robinmauritzrm@gmail.com>
2022-11-01 13:49:32 +0700

    ci: Add auto publish system
    ci: Fix wrong gh-action path
    
    ci: Swap from release based to tag based
    
    ci: Add manual trigger
    
    ci: Fix gradlew
    
    ci: Fix wrong URI

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+
+    `maven-publish`
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "XPathQueryBuilder"
+            url = uri("https://maven.pkg.github.com/RoganMatrivski/Kotlin-XPathQueryBuilder")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 group = "com.github.roganmatrivski"
